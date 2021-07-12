9b69d2e20ff30a11cd0563700a9f99f36d9a92df
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2021-07-12 16:06:52 +0300

    Add files via upload

---
+++ build/precache-manifest.720c3322e540eaa65e40970cbb0495c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be75755da3b93c8c85fc650ecf40276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4ae2debe4e9a0bb0ca9",
+    "url": "/static/css/main.85a51b4b.chunk.css"
+  },
+  {
+    "revision": "d81ed16d7542b321db11",
+    "url": "/static/js/2.bdaba24e.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.bdaba24e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ae2debe4e9a0bb0ca9",
+    "url": "/static/js/main.02c34ef7.chunk.js"
+  },
+  {
+    "revision": "2fa0f0a18ea9f778317c",
+    "url": "/static/js/runtime-main.79d8abac.js"
+  },
+  {
+    "revision": "ba99bb6fbf182f1a51abceb5ed5e53f5",
+    "url": "/static/media/GrandSlangRoman.ba99bb6f.otf"
+  },
+  {
+    "revision": "5433e572ad1189da67c4669f7043698a",
+    "url": "/static/media/aketch.5433e572.webp"
+  },
+  {
+    "revision": "cba5aeb4e51e0ee08bbc93fedc7ee81a",
+    "url": "/static/media/compressed-image.cba5aeb4.jpg"
+  }
+]);
